6fe46236351c86afead0f59fd89f6340e37ca031
William Ruben Salcedo <wrs225@cornell.edu>
2020-09-22 22:51:57 -0400

    Skeleton2

--- nav_algo/SailSensors/SailGPS.py
+++ nav_algo/SailSensors/SailGPS.py
@@ -1,1 +1,10 @@
+"""
+SailGPS implements a class used to connect the component to it's proper
+communication protocol. This class also implements functions to return raw data and
+turn on/off the sensors.
+"""
 
+class SailGPS:
+
+    def __init__(self):
+        return
