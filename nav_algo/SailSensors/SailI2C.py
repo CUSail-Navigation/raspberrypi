6eac72e42523738fda11866bed2177b5e8d694a1
wrs225 <wrs225@cornell.edu>
2020-09-26 15:21:21 -0400

    Added I2C class to Sensor Integration lib

---
+++ nav_algo/SailSensors/SailI2C.py
@@ -0,0 +1,10 @@
+"""
+This class is used to create a new instance of an I2C sensor module for communication.
+It also contains functions to communicate with the module.
+"""
+from smbus2 import SMBus, ic_msg
+
+class I2CObject:
+    def __init__(self):
+        return 0
+    
