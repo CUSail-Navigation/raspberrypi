775f33019a56f8a6708ac9507eee1e06239ff744
William Ruben Salcedo <wrs225@cornell.edu>
2020-10-02 22:18:48 -0400

    IMU constructors and lidar reading function

--- nav_algo/SailSensors/SailI2C.py
+++ nav_algo/SailSensors/SailI2C.py
@@ -5,19 +5,19 @@ It also contains functions to communicate with the module.
 from smbus2 import SMBus, ic_msg
 
 class I2CDevice:
-        
+
     def getDeviceAddress(self):
         return self.deviceAddress
-    
+
     def geti2cBusIndex(self):
         return self.i2cBusIndex
-    
+
     def geti2cBus(self):
         return self.i2cBus
-    
+
     def getName(self):
         return self.name
-    
+
     """
     initializes the class
     int deviceAddress (needs to be found using sudo i2cdetect -y 1)
@@ -30,11 +30,14 @@ class I2CDevice:
         self.i2cBus = SMBus(self.i2cBusIndex)
         self.name = name
         return
-        
+
     """
-    
+
     """
     def read_block_data(self,offset,byteNumber):
         return self.i2cBus.read_i2c_block_data(deviceAdddress,offset,byteNumber)
-        
-    
+
+    def i2cRdwr(self,msg):
+        send = i2c_msg.write(self.getDeviceAddress(),msg)
+        returnToSender = self.geti2cBus().i2c_rdwr(send)
+        return returnToSender
--- nav_algo/SailSensors/SailIMU.py
+++ nav_algo/SailSensors/SailIMU.py
@@ -4,7 +4,15 @@ communication protocol. This class also implements functions to return raw data 
 turn on/off the sensors.
 """
 
-class SailIMU:
+from smbus2 import SMBus, ic_msg
 
-    def __init__(self):
+class SailIMU(I2CDevice):
+
+    def __init__(self,deviceAddress,i2cBusIndex,name):
+        super().__init__(deviceAddress,i2cBusIndex,name)
         return
+
+    def i2c_read_lidar(self):
+        msg = [66,1,67]
+        data = self.i2cRdwr(msg)
+        return data
