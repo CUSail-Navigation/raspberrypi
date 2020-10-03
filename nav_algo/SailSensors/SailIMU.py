2a34f9c631d278ebc668ed93a5eff3b1a2a1e021
William Ruben Salcedo <wrs225@cornell.edu>
2020-10-02 23:16:33 -0400

    Add dictionary to IMU class so commands could be expressed as strings

--- nav_algo/SailSensors/SailIMU.py
+++ nav_algo/SailSensors/SailIMU.py
@@ -7,12 +7,17 @@ turn on/off the sensors.
 from smbus2 import SMBus, ic_msg
 
 class SailIMU(I2CDevice):
+    imuCommands = {
+    "readAccelerometerRaw": 66,
+    "readOrientationQuaternion": 1,
+    "readCompassRaw": 67
+    }
 
     def __init__(self,deviceAddress,i2cBusIndex,name):
         super().__init__(deviceAddress,i2cBusIndex,name)
         return
 
     def i2c_read_lidar(self):
-        msg = [66,1,67]
+        msg = [imuCommands["readAccelerometerRaw"],imuCommands["readOrientationQuaternion"],imuCommands["readCompassRaw"]]
         data = self.i2cRdwr(msg)
         return data
