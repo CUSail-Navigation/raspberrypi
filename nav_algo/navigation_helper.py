600aa3f5b6eafad77b4a597463f392a379dcb170
nlc62 <nlc62@cornell.edu>
2020-10-29 00:19:04 -0400

    Bug fixes in stationkeeping

--- nav_algo/navigation_helper.py
+++ nav_algo/navigation_helper.py
@@ -90,12 +90,25 @@ def endurance():
     pass
 
 
-def stationKeeping(waypoints, circle_radius, state):
+def stationKeeping(waypoints, circle_radius, state, opt_angle=45):
     if state == "ENTRY":
         stationKeepingWaypoints = []  # Necessary waypoints
         # entry point to the square
-        square_entry = waypoints[0].midpoint(waypoints[1])
-        stationKeepingWaypoints.append(square_entry)
+        square_entries = [waypoints[0].midpoint(waypoints[1]),
+                        waypoints[1].midpoint(waypoints[2]),
+                        waypoints[2].midpoint(waypoints[3]),
+                        waypoints[3].midpoint(waypoints[0])]
+        curr_pos = boat.getPosition()
+        shortest_dist = min((curr_pos.xyDist(square_entries[0]), square_entries[0]),
+                            (curr_pos.xyDist(
+                                square_entries[1]), square_entries[1]),
+                            (curr_pos.xyDist(
+                                square_entries[2]), square_entries[2]),
+                            (curr_pos.xyDist(
+                                square_entries[3]), square_entries[3]),
+                            key=lambda x: x[0])
+        stationKeepingWaypoints.append(shortest_dist[1])
+
         # center of the sqaure
         center = waypoints[0].midpoint(waypoints[2])
         stationKeepingWaypoints.append(center)
@@ -104,139 +117,57 @@ def stationKeeping(waypoints, circle_radius, state):
     elif state == "KEEP":
         # TODO: check with Will/Courtney on how to access yaw
         # downwind=0=clockwise,
-        optimal_angle = 45  # TODO is this ok?
-        radian_45=math.pi/4
+        keep_waypoints = []
+        # radian_angle = math.radians(opt_angle)
         x_coord = boat.getPosition().x
         y_coord = boat.getPosition().y
         boat_direction = boat.sensors.yaw
         if boat.sensors.wind_direction >= 180:
             # move ccw
-            loop_direction=1
+            loop_direction = 1
         else:
             # move cw
-            loop_direction=-1
-        #compute angle of first waypoint    
-        first_angle=boat_direction+optimal_angle*loop_direction
-        if (first_angle<0):
-            first_angle+=360   
-        #convert to radians for computation of other waypoints using trig    
-        radian_first_angle=first_angle*math.pi/180
+            loop_direction = -1
+        # compute angle of first waypoint
+        first_angle = boat_direction + opt_angle * loop_direction
+        if (first_angle < 0):
+            first_angle += 360
+        # convert to radians for computation of other waypoints using trig
+        first_angle_rad = math.radians(first_angle)
 
         for i in range(4):
-            #place 4 waypoints each 90 deg apart; when angle>2pi, trig functions know to shift input to be in range 
-            input_angle=radian_first_angle+loop_direction*i*radian_45
-            waypoints.append((x_coord+circle_radius*math.cos(input_angle)),(y_coord+circle_radius*math.sin(input_angle))
-        return waypoints
+            # place 4 waypoints each 90 deg apart; when angle>2pi, trig functions know to shift input to be in range
+            input_angle = first_angle_rad + \
+                loop_direction * i * math.radian(90)
+            keep_waypoints.append((x_coord+circle_radius*math.cos(input_angle)), (y_coord+circle_radius*math.sin(input_angle))
 
-
-    elif state == "OLD_KEEP":
-        #may be used later; will delete when we know we won't use it                     
-        x_coord = boat.getPosition().x
-        y_coord = boat.getPosition().y
-        pos = circle_radius * math.sqrt(2) / 2
-        way45 = ((pos) + x_coord, (pos) + y_coord)
-        way135 = (-(pos) + x_coord, (pos) + y_coord)
-        way225 = (-(pos) + x_coord, -(pos) + y_coord)
-        way315 = ((pos) + x_coord, -(pos) + y_coord)
-        boat_direction = boat.sensors.yaw
-        wind_direction = boat.sensors.wind_direction
-        angles = [45, 135, 225, 315]
-        between_45_and_90 = []  # minimum of all 4 angles
-        diff_180 = []
-        curr_min = float("inf")
-        last_angle = 0
-        last_min = float("inf")
-        for angle in angles:
-            diff = abs(angle - boat_direction)
-            second = abs(180 - boat_direction)
-            if diff >= 45 and diff <= 90:
-                between_45_and_90.append(angle)
-            if second <= 45:
-                diff_180.append(angle)
-            if diff < last_min:
-                last_angle = angle
-                last_min = diff
-        first = 0
-        second = 0
-        third = 0
-        fourth = 0
-        circle_waypoints_ccw
-        if len(between_45_and_90) > 1:
-            angle1 = between_45_and_90[0]
-            angle2 = between_45_and_90[1]
-            if angle1 == 45 and angle2 == 315:
-                if boat.sensors.wind_direction > 180:
-                    first = 45
-                    second = 135
-                    third = 225
-                    fourth = 315
-                else:
-                    first = 315
-                    second = 225
-                    third = 135
-                    fourth = 45
-            elif angle1 == 45 and angle2 == 135:
-                if boat.sensors.wind_direction > 180:
-                    first = 135
-                    second = 225
-                    third = 315
-                    fourth = 45
-                else:
-                    first = 45
-                    second = 315
-                    third = 225
-                    fourth = 135
-            elif angle1 == 135 and angle2 == 225:
-                if boat.sensors.wind_direction > 180:
-                    first = 225
-                    second = 315
-                    third = 45
-                    fourth = 135
-                else:
-                    first = 135
-                    second = 45
-                    third = 315
-                    fourth = 225
-            elif angle1 == 225 and angle2 == 315:
-                if boat.sensors.wind_direction > 180:
-                    first = 315
-                    second = 45
-                    third = 235
-                    fourth = 225
-                else:
-                    first = 225
-                    second = 135
-                    third = 45
-                    fourth = 315
-
-        circle_waypoints = [first, second, third, fourth]
-        waypoints = circle_waypoints
-        return circle_waypoints
+        waypoints=keep_waypoints
+        return keep_waypoints
 
     elif state == "EXIT":
         # corner waypoint order: NW, NE, SE, SW
         # TODO: ask Courtney about the units of x-y coord
-        units_away = 10
+        units_away=10
         # north exit
-        north_exit = waypoints[0].midpoint(waypoints[1])
+        north_exit=waypoints[0].midpoint(waypoints[1])
         north_exit.y += units_away
         # east exit
-        east_exit = waypoints[1].midpoint(waypoints[2])
+        east_exit=waypoints[1].midpoint(waypoints[2])
         east_exit.x += units_away
         # south exit
-        south_exit = waypoints[2].midpoint(waypoints[3])
+        south_exit=waypoints[2].midpoint(waypoints[3])
         south_exit.y -= units_away
         # west exit
-        west_exit = waypoints[0].midpoint(waypoints[3])
+        west_exit=waypoints[0].midpoint(waypoints[3])
         west_exit.x -= units_away
         # exit waypoint order in list: N, E, S, W
-        curr_pos = boat.getPosition()
-        shortest_dist = min((curr_pos.xyDist(north_exit), north_exit),
+        curr_pos=boat.getPosition()
+        shortest_dist=min((curr_pos.xyDist(north_exit), north_exit),
                             (curr_pos.xyDist(east_exit), east_exit),
                             (curr_pos.xyDist(south_exit), south_exit),
                             (curr_pos.xyDist(west_exit), west_exit),
                             key=lambda x: x[0])
-        waypoints = [shortest_dist[1]]
+        waypoints=[shortest_dist[1]]
         return
 
 
