c0546c3337321592dd514e37601be7f31063aa8d
William Ruben Salcedo <wrs225@cornell.edu>
2020-11-02 11:37:30 -0500

    Fixes for servo to work

--- nav_algo/servo.py
+++ nav_algo/servo.py
@@ -2,15 +2,20 @@ from RPIO import PWM
 
 class servo:
 
+    TAIL_MAX_ANGLE = 148
+    TAIL_MIN_ANGLE = 0
     SAIL_MAX = 0
     SAIL_MIN = 0
+
+    TAIL_MAX_ANGLE = 60
+    TAIL_MIN_ANGLE = 0
     TAIL_MAX = 1670
     TAIL_MIN = 858
 
     """
     instantiates the class. tailPin is the otherwise unused GPIO pin on the pi where the
     tail servo is connected, and sailPin is the otherwise unused GPIO pin where the sail
-    servo is connected. 
+    servo is connected.
     """
     def __init__(self,tailPin,sailPin):
         self.currentTail = 0
@@ -21,20 +26,22 @@ class servo:
         return
 
     """
-    Just instantiate the class then enter an angle into "servo"_angle as an int 0->180 degrees
+    Just instantiate the class then enter an angle into "servo"_angle as an
+    int TAIL_MIN_ANGLE -> TAIL_MAX_ANGLE degrees
     """
     def setTail(self, tail_angle):
-        mapAngle = (tail_angle / 180) * (TAIL_MAX - TAIL_MIN) + 858
-        self.servoControl.set_servo(tailPin,mapAngle)
+        mapAngle = (tail_angle / (servo.TAIL_MAX_ANGLE - servo.TAIL_MIN_ANGLE)) * (servo.TAIL_MAX - servo.TAIL_MIN) + servo.TAIL_MIN
+        self.servoControl.set_servo(self.tailPin,mapAngle)
         self.currentTail = tail_angle
         return
 
     """
-    Just instantiate the class then enter an angle into "servo"_angle as an int 0->180 degrees
+    Just instantiate the class then enter an angle into "servo"_angle as an
+    int SAIL_MIN_ANGLE -> SAIL_MAX_ANGLE degrees
     """
     def setSail(self, sail_angle):
-        mapAngle = (sail_angle / 180) * (SAIL_MAX - SAIL_MIN) + 858
-        self.servoControl.set_servo(sailPin,mapAngle)
+        mapAngle = (sail_angle / (servo.SAIL_MAX_ANGLE - servo.SAIL_MIN_ANGLE)) * (servo.SAIL_MAX - servo.SAIL_MIN) + servo.SAIL_MIN
+        self.servoControl.set_servo(self.sailPin,mapAngle)
         self.currentSail = sail_angle
         return
 
