3f68e4a5ab5a6917a63ab089911c17da415f8586
Grace Wang <gzw2@cornell.edu>
2024-02-10 14:14:37 -0500

    implemented skeleton of sailing algo

--- nav_algo/basic_algo.py
+++ nav_algo/basic_algo.py
@@ -8,12 +8,101 @@ class BasicAlgo:
     refactor if it's ever going to be used.
     """
     def __init__(self):
-        pass
+        # self.rudderAngle = 0 covered in servos
+        # self.sailAngle = 0 covered in servos
+        self.headingDir = 0
+        self.windDir = 0
+        # self.distToDest = 0 unsure if 
+        self.tacking = False
+        self.tackingPoint = None
+        self.tackingDuration = 0
+        self.currLoc = None
+        self.currDest = None 
     
-    def step(self, boat : b.BoatController, waypoint):
-        intended_angle = newSailingAngle(boat, waypoint)
+    def updateSensors(self, boat : b.BoatController, waypoint):
+        """
+        Updates the fields given by the sensor outputs. (Update after knowing 
+        what angle type). Figure out what parameters 
+        """ 
+        self.headingDir = 0 # set 
+        self.windDir = 0 # set
+        self.currLoc = None # set
+    
+    def setSail(self):
+        """
+        Calculate and set the new sail angle given ... Figure out what parameters are needed
+        """
+        if 30 < self.windDir < 180:
+            self.servos.setSail(round((7/15)*self.windDir + 186)*5)
+        elif 180 < self.windDir < 330:
+            self.servos.setSail(round((7/15)*self.windDir + 6)*5)
 
-        abs_wind_dir = boat.sensors.wind_direction # TODO is this right?
-        yaw = boat.sensors.yaw
-        sail, rudder = getServoAnglesImpl(abs_wind_dir, yaw, intended_angle)
-        return sail, rudder
+    def setRudder(self):
+        """
+        Calculte and set the new rudder angle given ...
+        """
+        # calculate based on whether we're tacking or not. if yes, calculate 
+        # direction to tacking point, if not, calcualte direction to dest. 
+        if self.tacking:
+            self.servos.setTail(round(.25 * (self.tackingPoint-self.headingDir))*5)
+        else:
+            self.servos.setTail(round(.25 * (self.currDest-self.headingDir))*5)
+
+    def inNoGo(self):
+        """
+        Checks if the boat is currently in the no go zone (within )
+        """
+        if abs(self.headingDir - self.windDir) < 30 or abs(self.headingDir - self.windDir) > 330:
+            return True
+        else:
+            return False
+    
+    def calcualteTP(self, currentLocation):
+        """
+        Calcualte tacking point to begin tacking. uses winddir + dest
+        """
+        pass 
+
+    def step(self, currentLoc, destination):
+        """
+        Sail. Todo: implement time buffer for tacking. if tacking for > 2 mins?
+        If we call this function every 4 seconds --> tackingDur > 30
+        """
+        self.updateSensors()
+        self.currDest = destination
+        # check if current location is within range of destination with euclidian norm
+        if self.tacking:
+            if not self.inNoGo():
+                if self.tackingDuration > 30:
+                    # only get out of tacking after 2 mins
+                    self.tacking = False
+                    self.tackingDuration = 0
+                else:
+                    self.tackingDuration += 1
+            else:
+                self.tackingDuration += 1   
+        else:
+            if self.inNoGo():
+                self.tacking = True
+                self.tackingDuration = 0
+                self.tackingPoint = self.calculateTP(currentLoc)
+        self.setSail()
+        self.setRudder()
+            
+
+
+        
+
+
+
+
+
+
+    # Old step func
+    # def step(self, boat : b.BoatController, waypoint):
+    #     intended_angle = newSailingAngle(boat, waypoint)
+
+    #     abs_wind_dir = boat.sensors.wind_direction # TODO is this right?
+    #     yaw = boat.sensors.yaw
+    #     sail, rudder = getServoAnglesImpl(abs_wind_dir, yaw, intended_angle)
+    #     return sail, rudder
