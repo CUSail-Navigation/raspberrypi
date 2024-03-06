0e3318929b0a416e7ab5cb4684540fafb7c07150
Grace Wang <gzw2@cornell.edu>
2024-03-06 16:34:55 -0500

    Integrated existing sensor reading calls

--- nav_algo/basic_algo.py
+++ nav_algo/basic_algo.py
@@ -20,91 +20,107 @@ class BasicAlgo:
         self.currLoc = None
         self.currDest = None 
     
-    def updateSensors(self, boat : b.BoatController, waypoint):
-        """
-        Updates the fields given by the sensor outputs. (Update after knowing 
-        what angle type). Figure out what parameters 
-        """ 
-        self.headingDir = 0 # set 
-        self.windDir = 0 # set
-        self.currLoc = None # set
+    # def updateSensors(self, boat : b.BoatController, waypoint):
+    #     """
+    #     Updates the fields given by the sensor outputs. (Update after knowing 
+    #     what angle type). Figure out what parameters 
+    #     """ 
+    #     self.headingDir = 0 # set 
+    #     self.windDir = 0 # set
+    #     self.currLoc = None # set
+    def normalize_angle(angle):
+        return angle % 360
     
-    def setSail(self):
+    def calibrate_wind_direction(wind_direction, heading_direction):
+        return (wind_direction - heading_direction) % 360
+    
+    def setSail(windDir, currHead):
         """
         Calculate and set the new sail angle given ... Figure out what parameters are needed
         """
-        if 30 < self.windDir < 180:
+        cWindDir = BasicAlgo.calibrate_wind_direction(windDir, currHead)
+        if 30 < cWindDir < 180:
             # self.servos.setSail(round((7/15)*self.windDir + 186)*5)
-            return round((7/15)*self.windDir + 186)*5
-        elif 180 < self.windDir < 330:
+            return round((7/15)*cWindDir + 186)*5
+        elif 180 < cWindDir < 330:
             # self.servos.setSail(round((7/15)*self.windDir + 6)*5)
-            return round((7/15)*self.windDir + 6)*5
+            return round((7/15)*cWindDir + 6)*5
 
-    def setRudder(self):
+    def setRudder(tacking, tackingPoint, headingDir, currDest):
         """
         Calculte and set the new rudder angle given ...
         """
         # calculate based on whether we're tacking or not. if yes, calculate 
         # direction to tacking point, if not, calcualte direction to dest. 
-        if self.tacking:
+        if tacking:
             # self.servos.setTail(round(.25 * (self.tackingPoint-self.headingDir))*5)
-            return round(.25 * (self.tackingPoint-self.headingDir))*5
+            return round(.25 * (tackingPoint-headingDir))*5
         else:
             # self.servos.setTail(round(.25 * (self.currDest-self.headingDir))*5)
-            return round(.25 * (self.currDest-self.headingDir))*5
+            return round(.25 * (currDest-headingDir))*5
 
-    def inNoGo(self):
+    def inNoGo(headingDir, windDir):
         """
         Checks if the boat is currently in the no go zone (within )
         """
-        if abs(self.headingDir - self.windDir) < 30 or abs(self.headingDir - self.windDir) > 330:
+        if abs(headingDir - windDir) < 30 or abs(headingDir - windDir) > 330:
             return True
         else:
             return False
+        
+    def calculateDistToDest(currLoc, destLoc):
+        """
+        Calculates the euclidean distance to the destination
+        """
+        cx, cy = currLoc[0], currLoc[1]
+        dx, dy = destLoc[0], destLoc[1]
+        return math.sqrt((cx - dx)**2 + (cy - dy)**2)
     
-    def calcualteTP(self, currentLocation):
+    def calcualteTP(currentLocation, destination, windDirection):
         """
         Calcualte tacking point to begin tacking. uses winddir + dest
         Assuming that the boat is heading towards the positive x-axis and the destination
         """
-        x = self.currentLoc[0]
-        y = self.currentLoc[1]
-        windDir = self.windDir % 360
+        x = currentLocation[0]
+        y = currentLocation[1]
+        dist2Dest = BasicAlgo.calculateDistToDest(currentLocation, destination)
+        windDir = windDirection % 360
         if windDir >= 0 and windDir <= 30:
-            x_TP = x + self.distToDest*np.cos(np.deg2rad(45-windDir))*np.sin(np.deg2rad(45+windDir))
-            y_TP = y - self.distToDest*np.cos(np.deg2rad(45-windDir))*np.cos(np.deg2rad(45+windDir))
-            self.tackingPoint = (x_TP, y_TP)
+            x_TP = x + dist2Dest*np.cos(np.deg2rad(45-windDir))*np.sin(np.deg2rad(45+windDir))
+            y_TP = y - dist2Dest*np.cos(np.deg2rad(45-windDir))*np.cos(np.deg2rad(45+windDir))
+            tackingPoint = (x_TP, y_TP)
         elif windDir >= 330 and windDir <= 359:
             windDir = 360 - windDir
-            x_TP = x + self.distToDest*np.cos(np.deg2rad(45-windDir))*np.sin(np.deg2rad(45+windDir))
-            y_TP = y + self.distToDest*np.cos(np.deg2rad(45-windDir))*np.cos(np.deg2rad(45+windDir))
-            self.tackingPoint = (x_TP, y_TP)
-        return self.tackingPoint
+            x_TP = x + dist2Dest*np.cos(np.deg2rad(45-windDir))*np.sin(np.deg2rad(45+windDir))
+            y_TP = y + dist2Dest*np.cos(np.deg2rad(45-windDir))*np.cos(np.deg2rad(45+windDir))
+            tackingPoint = (x_TP, y_TP)
+        return tackingPoint
 
-    def step(self, currentLoc, destination):
+    def step(currentLoc, destination, tacking, tpoint, tduration, headingDir, windDir):
         """
         Sail. Todo: implement time buffer for tacking. if tacking for > 2 mins?
         If we call this function every 4 seconds --> tackingDur > 30
         """
-        self.updateSensors()
-        self.currDest = destination
+        # self.updateSensors()
+        currDest = destination
         # check if current location is within range of destination with euclidian norm
-        if self.tacking:
-            if not self.inNoGo():
-                if self.tackingDuration > 30:
+        if tacking:
+            #  Finding 
+            if not BasicAlgo.inNoGo(headingDir, windDir):
+                if tduration > 30:
                     # only get out of tacking after 2 mins
-                    self.tacking = False
-                    self.tackingDuration = 0
+                    tacking = False
+                    tduration = 0
                 else:
-                    self.tackingDuration += 1
+                    tduration += 1
             else:
-                self.tackingDuration += 1   
+                tduration += 1   
         else:
-            if self.inNoGo():
-                self.tacking = True
-                self.tackingDuration = 0
-                self.tackingPoint = self.calculateTP(currentLoc)
-        return self.setSail(), self.setRudder()
+            if BasicAlgo.inNoGo(headingDir, windDir):
+                tacking = True
+                tduration = 0
+                tpoint = BasicAlgo.calculateTP(currentLoc)
+        return BasicAlgo.setSail(windDir, headingDir), BasicAlgo.setRudder(tacking, tpoint, headingDir, destination), tacking, tpoint, tduration
             
 
 
