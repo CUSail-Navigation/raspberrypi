18c15ffaed0c1e7890b2d5918bd5864aca4f9147
Nikil Shyamunder <nikil.shyamsunder@gmail.com>
2024-03-09 11:47:21 -0500

    removed class

--- nav_algo/event_tests/search_test.py
+++ nav_algo/event_tests/search_test.py
@@ -9,27 +9,25 @@ from nav_algo.navigation_utilities import *
 # search import search_helper
 from nav_algo.event_helper.search_event.search_helper import searchHelper
 
+print("Hello")
+print("Starting test for search.")
 
-class SearchTest:
+waypoints = [(0, 0)]
 
-    def __init__(self):
-        print("Starting test for search.")
+plt.figure()
+xs = [w[0] for w in self.waypoints]
+ys = [w[1] for w in self.waypoints]
+plt.plot(xs, ys, 'o', color="red")
 
-        self.waypoints = [(0, 0)]
+gen_waypoints = search_helper(self.waypoints)
+xs = [w[0] for w in gen_waypoints]
+ys = [w[1] for w in gen_waypoints]
+xss = np.array(xs)
+yss = np.array(ys)
+for i in range(len(xs)):
+    plt.plot(xs[i], ys[i], '-x', color='black', linewidth=2)
+    plt.quiver(xss[:-1], yss[:-1], xss[1:]-xss[:-1], yss[1:]-yss[:-1],
+                scale_units='xy', angles='xy', scale=1, color='black', width=0.005)
 
-        plt.figure()
-        xs = [w[0] for w in self.waypoints]
-        ys = [w[1] for w in self.waypoints]
-        plt.plot(xs, ys, 'o', color="red")
+plt.show()
 
-        gen_waypoints = search_helper(self.waypoints)
-        xs = [w[0] for w in gen_waypoints]
-        ys = [w[1] for w in gen_waypoints]
-        xss = np.array(xs)
-        yss = np.array(ys)
-        for i in range(len(xs)):
-            plt.plot(xs[i], ys[i], '-x', color='black', linewidth=2)
-            plt.quiver(xss[:-1], yss[:-1], xss[1:]-xss[:-1], yss[1:]-yss[:-1],
-                       scale_units='xy', angles='xy', scale=1, color='black', width=0.005)
-
-        plt.show()
